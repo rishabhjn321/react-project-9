f4d2130cfd59c11e7c7e2b608613eeb176a354c8
rishabhjn321 <93904354+rishabhjn321@users.noreply.github.com>
2022-02-07 08:52:53 +0530

    Add files via upload

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? '0' + hex : hex;// how to convert rgb to hex
+  }
+  
+  function rgbToHex(r, g, b) {
+    return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  }
+  
+  export default rgbToHex;
